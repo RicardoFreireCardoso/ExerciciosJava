dc9d176e6c4760fd7948b639a59a31cc79eb5d19
Ricardo Freire <ricardo10.freire@gmail.com>
2020-10-28 11:31:58 -0300

    finalizando

--- Java11Lts/src/Seçao5/Ex41.java
+++ Java11Lts/src/Seçao5/Ex41.java
@@ -6,9 +6,9 @@ public class Ex41 {
 		Scanner in = new Scanner (System.in);
 		double resistencia, r1, r2;
 		do {
-			System.out.println("Informe a primeira resistencia");
+			System.out.println("Informe a primeira resistencia:");
 			r1 = in.nextDouble();
-			System.out.println("Informe a segunda resistencia");
+			System.out.println("Informe a segunda resistencia:");
 			r2 = in.nextDouble();
 			if(r1 <=0 || r2 <= 0) {
 				System.out.println("Área inválida");
